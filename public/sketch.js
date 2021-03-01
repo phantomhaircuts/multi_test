734b63397ff16f10c6126478545e7d909f6a735c
erik <itserikanderson@gmail.com>
2021-03-01 15:00:18 -0500

    lets gooooo

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,51 @@
-let socket;
+let clr
+let socket
 function setup() {
-    createCanvas(400, 400);
-    socket = io.connect('http://localhost:3000');
-  }
-  
-  function draw() {
-    background(220);
-    ellipse(mouseX, mouseY, 60, 60);
-  }
+	let h = 400
+	let w = 400
+	socket = io.connect('http://localhost:3000')
+  socket.on('mouse', newDrawing);
+  createCanvas(h, w);
+  background(51);
+  clr = random(360)
+  noStroke()
+}
+
+
+function displayDot(x, y, color, color2 = 100){
+	colorMode(HSB)
+	fill(color, 100, color2)
+	ellipse(x, y, 10)
+	colorMode(RGB)
+}
+
+function draw() {
+}
+function mousePressed(){
+	mouseDragged()
+}
+function mouseDragged() {
+	clr += 1
+	clr = upgradeColor(clr)
+	let data = {
+		x: mouseX,
+		y: mouseY,
+		color: clr
+	}
+	socket.emit('mouse', data);
+	console.log('sending:', mouseX +',', mouseY +',', clr)
+	noStroke()
+	displayDot(mouseX, mouseY, clr)
+}
+function newDrawing(data){
+	data.color = upgradeColor(data.color)
+	displayDot(data.x, data.y, data.color, 30)
+}
+function upgradeColor(c){
+	if (c < 0){
+		c = 360 - c
+	} else if(c > 360){
+		c = c % 360
+	}
+	return c
+}
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const app = express();
-let server = app.listen(3000);
-app.use(express.static('public'))
 
-const socket = require('socket.io');
-let io = socket(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
-      credentials: true,
-    },
-  });
+let express = require('express');
+let app = express();
+let host = 3000
+let server = app.listen(host)
 
-io.sockets.on('connection', newConnection);
+app.use(express.static('public'));
+
+console.log("Socket server is running. localhost:" + host)
+
+let socket = require('socket.io')
+let io = socket(server);
+
+io.sockets.on('connection', newConnection)
+
 function newConnection(socket){
-    console.log(socket.id);
+	console.log('connection:',	socket.id);
+	socket.on('mouse', mouseMsg);
+	
+	function mouseMsg(data) {
+		socket.broadcast.emit('mouse', data)
+		console.log(data)
+	}
 }
-
